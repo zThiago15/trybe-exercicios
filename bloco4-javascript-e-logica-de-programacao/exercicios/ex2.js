62023519a7279b58f5562029880b1c0023db36e2
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-02-02 18:17:08 -0300

    exercicio 2 - 2 -comparando o maior de dois numeros

---
+++ bloco4-javascript-e-logica-de-programacao/exercicios/ex2.js
@@ -0,0 +1,12 @@
+let num1 = 15;
+let num2 = 30;
+
+if(num1 > num2){
+    console.log(`${num1} é o maior`);
+
+} else if(num1 == num2) {
+    console.log("Os dois números são iguais");
+
+} else {
+    console.log(`${num2} é o maior`)
+}
