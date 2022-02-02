471ee1c0f10ccd5e75e697483047369ed9fd8bd9
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-02-02 18:33:47 -0300

    exercicios 4 e 5 - positivo ou negativo e angulos

---
+++ bloco4-javascript-e-logica-de-programacao/exercicios/ex4.js
@@ -0,0 +1,11 @@
+const numero = 0;
+
+if(numero > 0){
+    console.log("positive");
+
+} else if(numero < 0){
+    console.log("negative");
+
+} else {
+    console.log("zero")
+}
---
+++ bloco4-javascript-e-logica-de-programacao/exercicios/ex5.js
@@ -0,0 +1,11 @@
+let ang1 = 43;
+let ang2 = 50;
+let ang3 = 53;
+
+const somaAngulos = ang1 + ang2 + ang3;
+
+if(somaAngulos == 180){
+    console.log("true");
+} else {
+    console.log("false");
+}
