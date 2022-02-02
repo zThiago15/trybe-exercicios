0bf9e501a6f846c7ffa06e081831f5ff6e87f599
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-02-02 18:14:18 -0300

    fazendo ex1 - operadores

---
+++ bloco4-javascript-e-logica-de-programacao/exercicios/ex1.js
@@ -0,0 +1,17 @@
+const a = 10;
+const b = 5;
+
+// Soma
+console.log(a + b);
+
+// Subtração
+console.log(a - b);
+
+// Multiplicação
+console.log(a * b);
+
+// Divisão
+console.log(a / b);
+
+// Resto da divisão
+console.log(a % b);
