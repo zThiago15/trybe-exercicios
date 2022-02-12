24c1b7885cb12fe8f38cb215cb663109f2f393e2
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-02-11 22:05:44 -0300

    ex 4 - redirecionando usuario ao portfolio

--- bloco5-javascript-dom-eventos-e-web-storage/dia3-eventos/main.js
+++ bloco5-javascript-dom-eventos-e-web-storage/dia3-eventos/main.js
@@ -30,7 +30,6 @@ listItems[2].classList.remove("tech");
 let li1 = listItems[0];
 let inputAlter = document.querySelector("#input");
 
-
 inputAlter.addEventListener('keyup', function () {
 
   li1.innerText = inputAlter.value;
@@ -39,6 +38,10 @@ inputAlter.addEventListener('keyup', function () {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+
+myWebpage.addEventListener('dblclick', function () {
+  window.location.href = "https://zthiago15.github.io/";
+});
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
