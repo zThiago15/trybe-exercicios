984dc7146e3f06edb6cb2eb2140a78618cd0a22c
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-03-04 23:18:13 -0300

    finalizando bonus 2

--- bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia2-jses6-fluxo-de-excecao-e-objetos/parte3.js
+++ bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia2-jses6-fluxo-de-excecao-e-objetos/parte3.js
@@ -1,29 +1,29 @@
 const lesson1 = {
-    materia: 'Matemática',
-    numeroEstudantes: 20,
-    professor: 'Maria Clara',
-    turno: 'manhã',
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
 };
 
 const lesson2 = {
-    materia: 'História',
-    numeroEstudantes: 20,
-    professor: 'Carlos',
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
 };
 
 const lesson3 = {
-    materia: 'Matemática',
-    numeroEstudantes: 10,
-    professor: 'Maria Clara',
-    turno: 'noite',
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
 };
 
 // 1 - Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 
 const addTurno = (obj, chave, valor) => {
 
-    obj[chave] = valor;
-    return obj;
+  obj[chave] = valor;
+  return obj;
 
 }
 console.log(addTurno(lesson2, 'turno', 'noite'));
@@ -31,7 +31,7 @@ console.log(addTurno(lesson2, 'turno', 'noite'));
 // 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 
 const listarChaves = (obj) => {
-    return Object.keys(obj);
+  return Object.keys(obj);
 
 }
 
@@ -41,11 +41,11 @@ console.log(listarChaves(lesson2));
 // 3 - Crie uma função para mostrar o tamanho de um objeto.
 
 const tamanhoObj = (obj) => {
-    let tamanho = 0;
-    for (const key in obj) {
-        tamanho += 1;
-    }
-    return `Tamanho: ${tamanho}`;
+  let tamanho = 0;
+  for (const key in obj) {
+    tamanho += 1;
+  }
+  return `Tamanho: ${tamanho}`;
 }
 
 console.log(tamanhoObj(lesson1));
@@ -54,7 +54,7 @@ console.log(tamanhoObj(lesson1));
 
 const listarValores = (obj) => {
 
-    return Object.values(obj);
+  return Object.values(obj);
 }
 
 console.log(listarValores(lesson3));
@@ -63,39 +63,39 @@ console.log('');
 // 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons).
 
 const allLessons = {
-    lesson1: Object.assign({}, lesson1),
-    lesson2: Object.assign({}, lesson2),
-    lesson3: Object.assign({}, lesson3)
+  lesson1: Object.assign({}, lesson1),
+  lesson2: Object.assign({}, lesson2),
+  lesson3: Object.assign({}, lesson3)
 }
 console.log(allLessons);
 
 // 6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 
 function totalEstudantes(obj) {
-    const les1 = obj.lesson1.numeroEstudantes;
-    const les2 = obj.lesson2.numeroEstudantes;
-    const les3 = obj.lesson3.numeroEstudantes;
+  const les1 = obj.lesson1.numeroEstudantes;
+  const les2 = obj.lesson2.numeroEstudantes;
+  const les3 = obj.lesson3.numeroEstudantes;
 
-    const total = les1 + les2 + les3;
-    return total;
+  const total = les1 + les2 + les3;
+  return total;
 }
 
 console.log(totalEstudantes(allLessons));
 
 // 7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
 const getValueFromKey = (obj, key) => {
-    try {
-        const keys = Object.keys(obj);
+  try {
+    const keys = Object.keys(obj);
 
-        if (keys[key] == undefined) {
-            throw new Error(`Erro: Número inválido. Posições disponíveis: ${keys.length}`);
-        }
-
-        return keys[key];
-
-    } catch (erro) {
-        return erro.message;
+    if (keys[key] == undefined) {
+      throw new Error(`Erro: Número inválido. Posições disponíveis: ${keys.length}`);
     }
+
+    return keys[key];
+
+  } catch (erro) {
+    return erro.message;
+  }
 }
 
 console.log(getValueFromKey(lesson1, 3));
@@ -104,13 +104,13 @@ console.log(getValueFromKey(lesson1, 3));
 
 const checkKeyValue = (obj, key, value) => {
 
-    const propriedades = Object.entries(obj);
-    for (let i = 0; i < propriedades.length; i += 1) {
-        if (propriedades[i][0] == key && propriedades[i][1] == value) {
-            return true;
-        }
+  const propriedades = Object.entries(obj);
+  for (let i = 0; i < propriedades.length; i += 1) {
+    if (propriedades[i][0] == key && propriedades[i][1] == value) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 console.log(checkKeyValue(lesson3, 'materia', 'Matemática')); // true
@@ -120,16 +120,48 @@ console.log(checkKeyValue(lesson3, 'turno', 'manha')); // false
 // 1B - Crie uma função para contar quantos estudantes assistiram às aulas de Matemática. Use o objeto criado no exercício 
 
 const countStudents = (obj) => {
-    const data = Object.entries(obj);
-    let totalEstudantes = 0;
+  const data = Object.entries(obj);
+  let totalEstudantes = 0;
 
-    for (let i = 0; i < data.length; i += 1) {
-        totalEstudantes += data[i][1].numeroEstudantes;
-    }
+  for (let i = 0; i < data.length; i += 1) {
+    totalEstudantes += data[i][1].numeroEstudantes;
+  }
 
-    return totalEstudantes;
+  return totalEstudantes;
 }
 
 console.log(countStudents(allLessons));
 
 // 2B - Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5:
+
+//console.log(createReport(allLessons, 'Maria Clara'))
+/* {
+  professor: 'Maria Clara',
+  aulas: [ 'Matemática', 'Matemática' ],
+  estudantes: 30
+} */
+console.log('----------------');
+
+const generateReport = (obj, prof) => {
+  const data = Object.entries(obj);
+  const aulas = [];
+  let estudantes = 0;
+
+  for (let i = 0; i < data.length; i += 1) {
+    // checar professor
+    if (data[i][1].professor == prof) {
+
+      aulas.push(data[i][1].materia);
+      estudantes += data[i][1].numeroEstudantes;
+    }
+  }
+  const report = {
+    professor: prof,
+    aulas: aulas,
+    estudantes: estudantes
+  }
+
+  return report;
+}
+
+console.log(generateReport(allLessons, 'Carlos'));
