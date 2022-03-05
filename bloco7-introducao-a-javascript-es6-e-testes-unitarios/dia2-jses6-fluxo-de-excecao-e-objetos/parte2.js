490ca115f940926db988cc04259dff01dd25738c
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-03-04 21:14:55 -0300

    finalizando parte 2 e 3 dos exercicios

--- bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia2-jses6-fluxo-de-excecao-e-objetos/parte2.js
+++ bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia2-jses6-fluxo-de-excecao-e-objetos/parte2.js
@@ -34,16 +34,45 @@ const order = {
     },
 };
 
-const customerInfo = (order) => {
-    // Adicione abaixo as informações necessárias.
+// 1 - Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+// Note que o parâmetro da função já está sendo passado na chamada da função.
 
+const customerInfo = (order) => {
+
+
+    let endereco = Object.values(order.address);
+    console.log(endereco);
+
+    let deliverMessage = `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone ${order.phoneNumber}, `;
+
+    for (const value of endereco) {
+        deliverMessage += `${value}, `;
+    }
+    return deliverMessage
 }
 
-customerInfo(order);
+console.log(customerInfo(order));
+
+
+// 2 - Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+// Modifique o nome da pessoa compradora.
+// Modifique o valor total da compra para R$ 50,00.
+
 
 const orderModifier = (order) => {
-    // Adicione abaixo as informações necessárias.
 
+    // Modificações no objeto
+    order.name = 'Luiz Silva';
+    order.order.drinks.price = 5;
+
+    const bebida = order.order.drinks.coke.type;
+    const total = order.order.drinks.price + order.order.pizza.marguerita.price + order.order.pizza.pepperoni.price;
+
+
+    let orderInfo = `Olá ${order.name}, o total do seu pedido de marguerita, pepperoni e ${bebida} é $RS${total}`;
+
+
+    return orderInfo
 }
 
-orderModifier(order);
+console.log(orderModifier(order));
---
+++ bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia2-jses6-fluxo-de-excecao-e-objetos/parte3.js
@@ -0,0 +1,117 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+};
+
+const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+};
+
+const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+};
+
+// 1 - Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+const addTurno = (obj, chave, valor) => {
+
+    obj[chave] = valor;
+    return obj;
+
+}
+console.log(addTurno(lesson2, 'turno', 'noite'));
+
+// 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const listarChaves = (obj) => {
+    return Object.keys(obj);
+
+}
+
+console.log(listarChaves(lesson1));
+console.log(listarChaves(lesson2));
+
+// 3 - Crie uma função para mostrar o tamanho de um objeto.
+
+const tamanhoObj = (obj) => {
+    let tamanho = 0;
+    for (const key in obj) {
+        tamanho += 1;
+    }
+    return `Tamanho: ${tamanho}`;
+}
+
+console.log(tamanhoObj(lesson1));
+
+// 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const listarValores = (obj) => {
+
+    return Object.values(obj);
+}
+
+console.log(listarValores(lesson3));
+console.log('');
+
+// 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons).
+
+const allLessons = {
+    lesson1: Object.assign({}, lesson1),
+    lesson2: Object.assign({}, lesson2),
+    lesson3: Object.assign({}, lesson3)
+}
+console.log(allLessons);
+
+// 6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+function totalEstudantes(obj) {
+    const les1 = obj.lesson1.numeroEstudantes;
+    const les2 = obj.lesson2.numeroEstudantes;
+    const les3 = obj.lesson3.numeroEstudantes;
+
+    const total = les1 + les2 + les3;
+    return total;
+}
+
+console.log(totalEstudantes(allLessons));
+
+// 7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+const getValueFromKey = (obj, key) => {
+    try {
+        const keys = Object.keys(obj);
+
+        if (keys[key] == undefined) {
+            throw new Error(`Erro: Número inválido. Posições disponíveis: ${keys.length}`);
+        }
+
+        return keys[key];
+
+    } catch (erro) {
+        return erro.message;
+    }
+}
+
+console.log(getValueFromKey(lesson1, 3));
+
+// 8 - Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
+
+const checkKeyValue = (obj, key, value) => {
+
+    const propriedades = Object.entries(obj);
+    for (let i = 0; i < propriedades.length; i += 1) {
+        if (propriedades[i][0] == key && propriedades[i][1] == value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(checkKeyValue(lesson3, 'materia', 'Matemática')); // true
+console.log(checkKeyValue(lesson3, 'turno', 'manha')); // false
