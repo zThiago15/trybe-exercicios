6af00f37c8a60c615a1daf3aee63d7267448067d
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-03-07 19:06:34 -0300

    adicionando arquivos e terminando ex 1

---
+++ bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia3-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,17 @@
+const sum = require('./sum.js');
+
+test('if return sum(4, 5) is equal to 9', () => {
+    expect(sum(4, 5)).toBe(9);
+})
+
+test('if sum(0,0) is equal to 0', () => {
+    expect(sum(0, 0)).toBe(0);
+})
+
+test('if sum(4, "5") returns an error', () => {
+    expect(() => sum(4, "5")).toThrow();
+})
+
+test('if message is "parameters must be numbers" when called sum(4, "5")', () => {
+    expect(() => sum(4, "5")).toThrow('parameters must be numbers');
+})
