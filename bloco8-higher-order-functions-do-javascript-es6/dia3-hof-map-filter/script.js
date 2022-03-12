bbd608c585da40f9c0df808c7fca90ead9b154f5
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-03-12 00:11:14 -0300

    pequena alteracao para fazer merge

--- bloco8-higher-order-functions-do-javascript-es6/dia3-hof-map-filter/script.js
+++ bloco8-higher-order-functions-do-javascript-es6/dia3-hof-map-filter/script.js
@@ -101,7 +101,6 @@ console.log(nameAndAge(books));
 
 // 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
 
-
 function fantasyOrScienceFiction(arr) {
     // usar filter, para pegar genero de ficao e e fantasia
 
