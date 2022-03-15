b487134d3ed488fbaead85463ed9664d0dc30432
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-03-15 00:02:49 -0300

    finalizando exs do dia 5 e 6 do dia 8.4

--- bloco8-higher-order-functions-do-javascript-es6/dia4-javascript-es6-hof-reduce/exercise5.js
+++ bloco8-higher-order-functions-do-javascript-es6/dia4-javascript-es6-hof-reduce/exercise5.js
@@ -8,6 +8,24 @@ const names = [
 
 function containsA() {
     // escreva seu código aqui
+    // 17 letras maiusculuas
+
+    let lettersUppercase = 0;
+    let lettersLowercase = 0;
+
+    let idk = names.reduce((accumulator, current) => {
+        // verificar, as letras de cada palavra
+        for (const letter of current) {
+            if (letter === letter.toUpperCase()) {
+                lettersUppercase += 1;
+            } else if (letter === letter.toLowerCase()) {
+                lettersLowercase += 1;
+            }
+        }
+        return accumulator;
+    }, '')
+
+    return `Maísculas: ${lettersUppercase}. Minúsculas: ${lettersLowercase}`;
 }
 
 console.log(containsA());
