8413c9cbd5492b01428f5e55b33d26514d76a9a1
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-05-04 16:26:49 -0300

    testando eventos com userEvent e funcoes de type e click com o email

--- front-end/bloco14-react-testing-library/dia1-rtl-primeiros-passos/fixacao/testes-react/src/App.js
+++ front-end/bloco14-react-testing-library/dia1-rtl-primeiros-passos/fixacao/testes-react/src/App.js
@@ -1,15 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <label>
-        Email
-        <input type="email" />
-      </label>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      saveEmail: '',
+    }
+  }
+
+  handleClick = (email) => {
+    this.setState({
+      saveEmail: email, 
+      email: '',
+    })
+  }
+
+  handleChange = ({target: { value }}) => {
+    this.setState({
+      email: value,
+    })
+  }
+
+  render() {
+
+    const { saveEmail, email } = this.state;
+    return (
+      <div className="App">
+        <label>
+          Email
+          <input onChange={ this.handleChange } type="email" value={ email } />
+        </label>
+  
+        <button data-testid="id-send" value="Enviar" onClick={() => this.handleClick(email) }>Enviar</button>
+        <button data-testid="back-btn" value="Voltar">Voltar</button>
+
+        <h2 data-testid="id-email-user">{`Valor: ${saveEmail}`}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
