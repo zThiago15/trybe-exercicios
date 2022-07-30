a1a93cb3b8f2cf566bce58c99b4435c6ac165304
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-07-30 01:54:08 -0300

    feat: autenticacao e geracao de token nos endpoints

--- back-end/bloco22-node.js/dia4-express-http-com-nodejs/exercicios/server.js
+++ back-end/bloco22-node.js/dia4-express-http-com-nodejs/exercicios/server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+const { generateToken } = require('./generateToken');
+
 app.use(express.json()); // bodyParser nativo
 
 app.get('/ping', (req, res) => res.status(200).json({ message: 'pong' }));
 
 app.post('/hello', (req, res) => {
   const { name } = req.body;
+  const { authorization } = req.header;
+
+  if (authorization.length !== 16 || !authorization) { 
+    return res.status(401).json({ "message": 'Token inválido!' })
+  }
 
   res.status(200).json({ "message": `Hello, ${name}` });
 
@@ -15,6 +22,12 @@ app.post('/hello', (req, res) => {
 
 app.post('/greetings', (req, res) => {
   const { name, age } = req.body;
+
+  const { authorization } = req.header;
+
+  if (authorization.length !== 16 || !authorization) { 
+    return res.status(401).json({ "message": 'Token inválido!' })
+  }
 
   if (age > 17) {
     return res.status(200).json({ "message": `Hello, ${name}` });
@@ -25,6 +38,12 @@ app.post('/greetings', (req, res) => {
 
 app.put('/users/:name/:age', (req, res) => {
 
+  const { authorization } = req.header;
+
+  if (authorization.length !== 16 || !authorization) { 
+    return res.status(401).json({ "message": 'Token inválido!' })
+  }
+
   const { name, age } = req.params;
 
   return res.status(201).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` })
@@ -32,6 +51,12 @@ app.put('/users/:name/:age', (req, res) => {
 
 // simpsons
 app.get('/read-simpsons', (req, res) => {
+  const { authorization } = req.header;
+
+  if (authorization.length !== 16 || !authorization) { 
+    return res.status(401).json({ "message": 'Token inválido!' })
+  }
+
   try {
     const data = fs.readFileSync('./simpsons.json', 'utf-8');
     return res.status(200).json({ message: 'success' });
@@ -42,6 +67,12 @@ app.get('/read-simpsons', (req, res) => {
 
 app.get('/simpsons', (req, res) => {
   
+  const { authorization } = req.header;
+
+  if (authorization.length !== 16 || !authorization) { 
+    return res.status(401).json({ "message": 'Token inválido!' })
+  }
+
   try {
     const simpsons = fs.readFileSync('./simpsons.json', 'utf-8');
     return res.status(200).send(simpsons);
@@ -52,6 +83,12 @@ app.get('/simpsons', (req, res) => {
 });
 
 app.get('/simpsons/:id', (req, res) => {
+
+  const { authorization } = req.header;
+
+  if (authorization.length !== 16 || !authorization) { 
+    return res.status(401).json({ "message": 'Token inválido!' })
+  }
 
   const { id } = req.params;
 
@@ -64,10 +101,16 @@ app.get('/simpsons/:id', (req, res) => {
   }
 
   return res.status(200).json(simpson);
-})
+});
 
 app.post('/simpsons', (req, res) => {
   const { id, name } = req.body;
+
+  const { authorization } = req.header;
+
+  if (authorization.length !== 16 || !authorization) { 
+    return res.status(401).json({ "message": 'Token inválido!' })
+  }
 
   const data = JSON.parse(fs.readFileSync('./simpsons.json', 'utf-8'));
   
@@ -80,7 +123,19 @@ app.post('/simpsons', (req, res) => {
 
   return res.status(204).end();
 
-})
+});
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+
+
+  if (!email || !password || !firstName || !phone) {
+    return res.status(401).json({ message: 'missing fields' });
+  }
+
+  const token = generateToken();
+  return res.status(200).json({ token });
+});
 
 const port = 3001;
 app.listen(port, () => console.log(`Listening at port ${port}`));
