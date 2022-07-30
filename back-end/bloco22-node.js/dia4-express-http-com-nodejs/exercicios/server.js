7957eaca3c9879035134e326140c2c0792d4af5b
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-07-30 00:04:03 -0300

    feat: criando api greetings que retorna mensagem para o usuario

--- back-end/bloco22-node.js/dia4-express-http-com-nodejs/exercicios/server.js
+++ back-end/bloco22-node.js/dia4-express-http-com-nodejs/exercicios/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
-
 const app = express();
+
 
 app.use(express.json()); // bodyParser nativo
 
-app.get('/ping', (req, res) => res.status(200).json({ message: 'pong' }))
+app.get('/ping', (req, res) => res.status(200).json({ message: 'pong' }));
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
 
-const port = 3001
-app.listen(port, () => console.log(`Listening at port ${port}`))
+  res.status(200).json({ "message": `Hello, ${name}` });
+
+})
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if (age > 17) {
+    return res.status(200).json({ "message": `Hello, ${name}` });
+  }
+  
+  return res.status(401).json({ "message": "Unanthorized" });
+})
+
+const port = 3001;
+app.listen(port, () => console.log(`Listening at port ${port}`));
