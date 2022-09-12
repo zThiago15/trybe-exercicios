6bfe5127f0cf281cd9ce832d52000110edf9cd14
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-09-12 14:44:41 -0300

    feat: adicionando endpoints de buscar todos os livros e buscar por id

---
+++ back-end/bloco26-typescript/dia3-express-com-ts/fixar/express-com-typescript/controllers/books.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import BookService from '../services/books.service';
+
+class BooksController {
+  constructor(private bookService = new BookService()) { }
+
+  public getAll = async (_req: Request, res: Response) => {
+    const books = await this.bookService.getAll();
+    res.status(StatusCodes.OK).json(books);
+  };
+
+  public getById = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const book = await this.bookService.getById(id);
+
+    if (!book) {
+      return res.status(StatusCodes.NOT_FOUND).json({ message: 'Book not found!' });
+    }
+
+    return res.status(StatusCodes.OK).json(book);
+  }
+}
+
+export default BooksController;
---
+++ back-end/bloco26-typescript/dia3-express-com-ts/fixar/express-com-typescript/services/books.service.ts
@@ -0,0 +1,23 @@
+import connection from '../models/connection';
+import BookModel from '../models/book.model';
+import Book from '../interfaces/book.interface';
+
+class BookService {
+  public model: BookModel;
+
+  constructor() {
+    this.model = new BookModel(connection);
+  }
+
+  public async getAll(): Promise<Book[]> {
+    const books = await this.model.getAll();
+    return books;
+  }
+
+  public async getById(id: number): Promise<Book> {
+    const book = await this.model.getById(id);
+    return book;
+  }
+}
+
+export default BookService;
