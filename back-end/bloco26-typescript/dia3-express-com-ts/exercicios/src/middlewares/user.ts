d2c116ed20e92cbe38743a6cf584e2ea05cc234e
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-09-13 13:25:18 -0300

    feat: validando nome, email e senha

--- back-end/bloco26-typescript/dia3-express-com-ts/exercicios/src/middlewares/user.ts
+++ back-end/bloco26-typescript/dia3-express-com-ts/exercicios/src/middlewares/user.ts
@@ -8,6 +8,18 @@ const validationUser = (req: Request, res: Response, next: NextFunction) => {
     return res.status(StatusCodes.NOT_FOUND).json({ message: `Preencher campos obrigatórios` })
   }
 
+  if (name.length < 3) 
+    return res.status(StatusCodes.NOT_ACCEPTABLE).json({ message: 'Nome deve ter no mínimo 3 caracteres' });
+  
+  if (password.length < 6 || password.length > 12) 
+    return res.status(StatusCodes.NOT_ACCEPTABLE).json({ message: 'Senha tem que ter no mínimo 6 caracteres e no máximo 12' })
+
+    
+  const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+  if (!regexEmail.test(email)) 
+    return res.status(StatusCodes.NOT_ACCEPTABLE).json({ message: 'E-mail inválido' });
+  
+
   next();
 }
 
