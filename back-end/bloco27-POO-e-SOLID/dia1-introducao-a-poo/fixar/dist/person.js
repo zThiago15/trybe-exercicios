25155dfac2158e8bf2129ae3dbf599f45fac033f
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-09-15 16:29:32 -0300

    feat: alterando atributos de TV para private, adicionando metodo getter e setter para '_connectedTo' e fazendo validacao na alteracao do seu valor, verificando se a conexao esta disponivel

---
+++ back-end/bloco27-POO-e-SOLID/dia1-introducao-a-poo/fixar/dist/person.js
@@ -0,0 +1,42 @@
+"use strict";
+class Person {
+    constructor(name, weight, age, height) {
+        this.name = name;
+        this._weight = weight;
+        this._age = age;
+        this.height = height;
+    }
+    // esta sintaxe permite acessar o valor retornado via person.getWeight()
+    getWeight() {
+        return this._weight;
+    }
+    // esta sintaxe permite acessar o age com person.age, como se fosse um atributo normal.
+    get age() {
+        return this._age;
+    }
+    set age(newAge) {
+        if (newAge > 0 && newAge < 200) {
+            this._age = newAge;
+        }
+    }
+    birthday() {
+        return this._age += 1;
+    }
+}
+const p1 = new Person('Maria', 171, 58, 19);
+const p2 = new Person('João', 175, 66, 18);
+console.log(p1.getWeight()); // 171
+// Acesso a método com getter para manipular atributo privado como se fosse público
+console.log(p1.age); // 58
+p1.name = 'Mariah';
+console.log(p1.name); // Mariah
+// // Acesso a método público que manipula atributo privado
+console.log(p1.birthday()); // 59
+console.log(p1.age); // 59
+// Acesso a método com setter para manipular atributo privado como se fosse público
+p2.age = 17;
+console.log(p2.age); // 17
+// Leitura de atributo readonly
+console.log(p1.height); // 19
+p2.age = 300;
+console.log(p2.age); // 17
--- back-end/bloco27-POO-e-SOLID/dia1-introducao-a-poo/fixar/src/tv.ts
+++ back-end/bloco27-POO-e-SOLID/dia1-introducao-a-poo/fixar/src/tv.ts
@@ -1,15 +1,29 @@
 class TV {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectedTo?: string;
+  private brand: string;
+  private size: number;
+  private resolution: string;
+  private connections: string[];
+  private _connectedTo: string;
 
-  constructor(b: string, s: number, r: string, con: string[]) {
+  constructor(b: string, s: number, r: string, con: string[], conTo: string) {
     this.brand = b;
     this.size = s;
     this.resolution = r;
     this.connections = con;
+    this._connectedTo = conTo;
+  }
+
+  get connectedTo() {
+    return this._connectedTo;
+  }
+
+  set connectedTo(con: string) {
+    if (this.connections.includes(con)) {
+      this._connectedTo = con;
+    } else {
+      console.log('Sorry, connection unavailable!');
+    }
+    
   }
 
   turnOn() {
@@ -18,5 +32,13 @@ class TV {
   }
 }
 
-const samsungTv = new TV('samsung', 45, '60 x 39', ['Ethernet', 'HDMI']);
-console.log(samsungTv);
+const samsungTv = new TV('samsung', 45, '60 x 39', ['Ethernet', 'HDMI'], 'HDMI');
+
+console.log(samsungTv.connectedTo); // hd
+
+// alterando atributo privado 'connectedTo' com uma conexão inválida
+samsungTv.connectedTo = 'HD'; // console.log -> 'Sorry, connection unavailable!'
+
+// alterando o atributo privado connectedTo com uma conexão válida!
+samsungTv.connectedTo = 'Ethernet'
+console.log(samsungTv.connectedTo); // Ethernet
