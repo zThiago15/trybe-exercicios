dc29fe5e173f4c2b49c718dc0b9002e1ef4167af
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-09-19 16:55:46 -0300

    feat: criando classe CharacterService que recebe uma instancia que implementa a interface IModel. Como os objetos das classes LocalDbModel e MockedDbModel

--- back-end/bloco27-POO-e-SOLID/dia3-polimorfismo/fixar/src/interface/interface.ts
+++ back-end/bloco27-POO-e-SOLID/dia3-polimorfismo/fixar/src/interface/interface.ts
@@ -1,14 +1,3 @@
-interface Character {
-  name: string;
-  skill: string;
-}
-
-interface DbCharacter extends Character {
-  id: number;
-}
-
-const db: DbCharacter[] = [];
-
 interface IModel {
   create(): void
   select(): void
@@ -18,26 +7,51 @@ interface IModel {
 
 class LocalDbModel implements IModel {
   create(): void {
-    
+    console.log('create');
   }
 
   select(): void {
-    
+    console.log('select');
   }
 
   update(): void {
-    
+    console.log('update');
   }
 
   delete(): void {
-    
+    console.log('delete');
+  }
+}
+
+class MockedDbModel implements IModel {
+  create(): void {
+    console.log('mock create');
+  }
+
+  select(): void {
+    console.log('mock select');
+  }
+
+  update(): void {
+    console.log('mock update');
+  }
+
+  delete(): void {
+    console.log('mock delete');
   }
 }
 
 class CharacterService {
   db: LocalDbModel;
 
-  constructor(dbModel: LocalDbModel) {
+  constructor(dbModel: IModel) {
     this.db = dbModel;
   }
 }
+
+const myDbMySQL = new LocalDbModel();
+const mockMySQL = new MockedDbModel();
+
+
+const characterService = new CharacterService(myDbMySQL);
+const characterService2 = new CharacterService(mockMySQL);
