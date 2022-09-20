2c666763b62d745ef67bf8ee66e21daa0c93f20c
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-09-20 01:20:33 -0300

    feat: ex6 - criando classe OrderRepository para representar o relatorio de vendas da lanchonete

--- back-end/bloco27-POO-e-SOLID/dia3-polimorfismo/exercicios/src/order.ts
+++ back-end/bloco27-POO-e-SOLID/dia3-polimorfismo/exercicios/src/order.ts
@@ -37,16 +37,22 @@ enum PaymentMethod {
 }
 
 class Order {
+  private _id: number;
   private _client: Person;
   private _items: number[];
   private _paymentMethod: PaymentMethod;
-  private _discount?: number;
+  private _discount: number;
 
   constructor(client: Person, items: number[], paymentMethod: PaymentMethod, discount: number) {
+    this._id = Math.floor(Date.now() * (Math.random() + 1));
     this._client = client;
     this._items = items;
     this._paymentMethod = paymentMethod;
     this._discount = discount;
+  }
+
+  get id(): number {
+    return this._id;
   }
 
   public get client(): Person {
@@ -91,9 +97,51 @@ class Order {
   }
 
   calculateTotalWithDiscount() {
-    if (this._discount) {
-      const priceItems = this.calculateTotal();
-      return priceItems - (priceItems * this._discount)
+    const priceItems = this.calculateTotal();
+    return priceItems - (priceItems * this._discount)
+    
+  }
+}
+
+class OrderRepository {
+  constructor( public orders: Order[] ) { }
+
+  addOrder(order: Order) {
+    this.orders.push(order);
+  }
+
+  removeOrder(order: string) {
+    const index = this.orders.findIndex((order) => order.id === order.id);
+    this.orders.splice(index, 1);
+  }
+
+  listByClient(value: Person) {
+    return this.orders.filter((order) => order.client === value);
+  }
+
+  listBySortedValue(sort: string): Order[] {
+    if (!['maior', 'menor'].includes(sort)) throw new Error('Valor de ordenação inválido.');
+
+    if (sort === 'menor') {
+      return this.orders.sort(
+        (orderA, orderB) => {
+          if (orderA.calculateTotalWithDiscount() > orderB.calculateTotalWithDiscount()) return 1;
+
+          if (orderA.calculateTotalWithDiscount() < orderB.calculateTotalWithDiscount()) return -1;
+
+          return 0;
+        },
+      );
     }
+
+    return this.orders.sort(
+      (orderA, orderB) => {
+        if (orderA.calculateTotalWithDiscount() > orderB.calculateTotalWithDiscount()) return -1;
+
+        if (orderA.calculateTotalWithDiscount() < orderB.calculateTotalWithDiscount()) return 1;
+
+        return 0;
+      },
+    );
   }
 }
