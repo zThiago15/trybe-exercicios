4ace43828516b6016d7fc1440d4b3dfb83881e0a
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-09-20 16:23:01 -0300

    feat: utilizando principio O de SOLID para garantir que possamos ter nota de corte diferentes dependendo da escola sem altererar o comportamento da funcao de aprovar estudantes

--- back-end/bloco27-POO-e-SOLID/dia4-principios-solid-SOD/fixar/src/index.ts
+++ back-end/bloco27-POO-e-SOLID/dia4-principios-solid-SOD/fixar/src/index.ts
@@ -6,10 +6,15 @@ type Discipline = {
   letterGrade?: string;
 };
 
+type School = {
+  name: string;
+  approvalGrade: number;
+};
+
 type Student = {
   name: string;
   disciplines: Discipline[];
-  school?: string;
+  school: School; // Agora não é mais uma string
 };
 
 /* Apoio para a função `getGradeLetter` */
@@ -40,9 +45,30 @@ const percentageGradesIntoLetters = (student: Student): Student => ({
   disciplines: student.disciplines.map(getLetterGrades),
 });
 
-/* "Determinar" */
-const approvedStudents = ({ disciplines }: Student): boolean =>
-  disciplines.every(({ grade }) => grade > 0.7);
+/* Fazemos a modificação da nossa função approvedStudents */
+const approvedStudents = ({ disciplines, school }: Student): boolean =>
+  disciplines.every(({ grade }) => (
+    grade >= school.approvalGrade
+));
+
+const studentsExample = [
+  {
+    name: 'Lee',
+    school: { name: 'Standard', approvalGrade: 0.7 },
+    disciplines: [
+      { name: 'matemática', grade: 0.8 },
+      { name: 'história', grade: 0.9 },
+    ],
+  },
+  {
+    name: 'Albus',
+    school: { name: 'Hogwarts', approvalGrade: 0.8 },
+    disciplines: [
+      { name: 'divination', grade: 0.8 },
+      { name: 'potions', grade: 0.9 },
+    ],
+  },
+];
 
 /* "Atualizar" */
 const updateApprovalData = (student: Student): void => {
@@ -58,6 +84,8 @@ function setApproved(students: Student[]): void {
     .filter(approvedStudents)
     .map(updateApprovalData);
 }
+
+setApproved(studentsExample);
 
 export {
   percentageGradesIntoLetters,
