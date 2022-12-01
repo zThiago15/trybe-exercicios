4ec160d68df278abde30cb917a6a33361cb186af
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-12-01 13:54:46 -0300

    test: testando falha

--- ciencia-da-computacao/dia3-testes/fixar/test_codigo.py
+++ ciencia-da-computacao/dia3-testes/fixar/test_codigo.py
@@ -1,4 +1,5 @@
-from codigo import is_odd
+import pytest
+from codigo import is_odd, divide
 
 def test_is_odd_returns_True_when_number_is_odd():
   'Retorna True se o número é ímpar'
@@ -7,3 +8,7 @@ def test_is_odd_returns_True_when_number_is_odd():
 def test_is_odd_returns_False_when_number_is_even():
   'Retorna False se o número é par'
   assert is_odd(10) is False
+
+def test_divide_when_other_number_is_zero_raises_an_exception():
+    with pytest.raises(ZeroDivisionError, match="division by zero"):
+        divide(2, 0)
