d2ba2c800271deef14dd352f442434994e18cac3
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-12-01 19:34:54 -0300

    feat: ex4 - validando uma lista de emails

---
+++ ciencia-da-computacao/dia3-testes/exercicios/ex4_validating_emails.py
@@ -0,0 +1,33 @@
+from ex3_validate_email import validate_email
+
+def filter_valid_emails(emails):
+    valid_emails = []
+    for email in emails:
+        try:
+            validate_email(email)
+        except ValueError as exc:
+            print(exc)
+        else:
+            valid_emails.append(email)
+    return valid_emails
+
+def test_if_dont_have_emails_returns_empty_list():
+    assert filter_valid_emails([]) == []
+
+
+def test_only_valid_emails():
+    emails = ["username@website.com"]
+    expected_emails = ["username@website.com"]
+    assert filter_valid_emails(emails) == expected_emails
+
+
+def test_invalid_emails_should_be_filtered():
+    emails = ["inv*alid@website.com"]
+    expected_emails = []
+    assert filter_valid_emails(emails) == expected_emails
+
+
+def test_valid_and_invalid_emails_returns_only_valids():
+    emails = ["username@website.com", "inv*alid@website.com"]
+    expected_emails = ["username@website.com"]
+    assert filter_valid_emails(emails) == expected_emails
